de026687a70f605fa4e42a1e4298ab8890c6ade0
HyperMax6 <99598582+HyperMax6@users.noreply.github.com>
2024-08-19 17:49:28 +0200

    added js

--- lib.js
+++ lib.js
@@ -2,4 +2,5 @@ const calcAddition = (v1, v2) => {
   return v1 + v2;
 };
 
-export { calcAddition };
+const textElement = document.querySelector("#text");
+textElement.innerHTML = calcAddition(5, 3);
